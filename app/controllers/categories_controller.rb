d2ade03d6bc174689fa5e00a2c2acc1d705ed7a7
zeptose <mcgrudy1232gmail.com>
2023-03-14 17:45:48 -0400

    controller and views setup

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,46 @@
 class CategoriesController < ApplicationController
-
-    
-end
+    before_action :set_category, only: [:edit, :update]
+    before_action :check_login
+    authorize_resource
+  
+    def index
+      @active_categories = Category.active.paginate(page: params[:page]).per_page(15)
+      @inactive_categories = Category.inactive.paginate(page: params[:page]).per_page(15)
+    end
+  
+    def new
+      @category = Category.new
+    end
+  
+    def create
+      @category = Category.new(category_params)
+      if @category.save
+        flash[:notice] = "Successfully added #{@category.name} to the system."
+        redirect_to categories_url
+      else
+        render action: 'new'
+      end
+    end
+  
+    def edit
+    end
+  
+    def update
+      if @category.update_attributes(category_params)
+        flash[:notice] = "Successfully updated #{@category}."
+        redirect_to categories_url
+      else
+        render action: 'edit'
+      end
+    end
+  
+    private
+      def category_params
+          params.require(:category).permit(:name, :active)
+      end
+  
+      def set_category
+          @category = Category.find(params[:id])
+      end
+  end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
+
+  resources :sessions 
+  # Semi-static page routes
+   get 'home', to: 'home#index', as: :home
+   get 'home/about', to: 'home#about', as: :about
+   get 'home/contact', to: 'home#contact', as: :contact
+   get 'home/privacy', to: 'home#privacy', as: :privacy
+   get 'search', to: 'search#search', as: :search
+ 
+  #  resources :users, only: [:edit, :index, :new, :create, :update]
+  #  get 'users/new', to: 'users#new', as: :signup
+  #  get 'user/edit', to: 'users#edit', as: :edit_current_user
+   get 'login', to: 'sessions#new', as: :login
+   get 'logout', to: 'sessions#destroy', as: :logout
+ 
+ 
+   resources :categories, only: [:edit, :index, :new, :create, :update]
+   resources :customers
+   resources :addresses
+   resources :orders
+   resources :items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
